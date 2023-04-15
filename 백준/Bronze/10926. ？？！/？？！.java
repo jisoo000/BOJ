90ca001e6e632768155873b0eb15f2f01cc35c21
Jisu <113496832+jisoo000@users.noreply.github.com>
2023-04-15 10:28:39 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String id = br.readLine();
+    String amazed = "??!";
+        System.out.println(id + amazed);
+    }
+}
