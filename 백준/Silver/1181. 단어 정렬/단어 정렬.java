c976f1efa69dc34709329958005196dc47848022
Jisu <113496832+jisoo000@users.noreply.github.com>
2023-04-16 15:20:01 +0900

    [Silver V] Title: 단어 정렬, Time: 360 ms, Memory: 19768 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,37 +1,41 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			int N = Integer.parseInt(br.readLine());
+			ArrayList<ArrayList<String>> lengthMemo = new ArrayList<>();
+			for(int i = 0 ; i <= 50 ; i++) {
+				lengthMemo.add(new ArrayList<String>());
+			}
+			
+			for(int i = 0 ; i < N ; i++) {
+				String line = br.readLine();
+				lengthMemo.get(line.length()).add(line);
+			}
+			for(int i = 1 ; i <= 50 ; i++) {
+				lengthMemo.get(i).sort(null);
+				String prev = "";
+				for(int j = 0 ; j < lengthMemo.get(i).size() ; j++) {
+					if(lengthMemo.get(i).get(j).equals(prev)) continue;
+					bw.write(prev = lengthMemo.get(i).get(j));
+					bw.newLine();
+				}
+			}
+			bw.flush();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-        Set<String> set = new HashSet<>();
-        String input;
-        for (int i = 0; i < n; i++) {
-            input = br.readLine().trim();
-            set.add(input);
-        }
-
-        TreeSet<String> sortedSet = new TreeSet<>(new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                // 길이가 짧은 순서 우선
-                if (s1.length() != s2.length()) {
-                    return s1.length() - s2.length();
-                }
-                // 길이가 같은 경우 사전 순으로 정렬
-                else {
-                    return s1.compareTo(s2);
-                }
-            }
-        });
-
-        sortedSet.addAll(set);
-
-        for (String s : sortedSet) {
-            System.out.println(s);
-        }
-    }
 }
